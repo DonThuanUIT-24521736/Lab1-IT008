e4023581438796008e0a60c3576423778665bfc1
DonThuanUIT-24521736 <24521736@gm.uit.edu.vn>
2025-09-28 10:12:48 +0700

    modify 3

--- Bai01/Program.cs
+++ Bai01/Program.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bai02
+namespace Bai01
 {
     internal class Program
     {
-        static bool isPrime(int x)
+        static void OutPutArr(int[] arr)
+        {
+            Console.Write("[ ");
+            foreach (int x in arr)
+            {
+                Console.Write(x + " ");
+            }
+            Console.WriteLine("]");
+        }
+        static int sumOddSum(int[] arr, ref bool check) // hàm tổng số lẻ 
+        {
+            int sum = 0;
+            foreach (int x in arr)
+            {
+                if (x % 2 != 0)
+                {
+                    check = true;
+                    sum += x;
+                }
+            }
+            return sum;
+        }
+        static bool isPrime(int x) // hàm kiểm tra số nguyên tố 
         {
             if (x < 2) return false;
             for (int i = 2; i * i <= x; i++)
@@ -17,37 +40,87 @@ namespace Bai02
             }
             return true;
         }
-
-        static void Input(out int n)
+        static int countPrime(int[] arr) // hàm đếm số nguyên tố trong mảng 
         {
-            n = int.Parse(Console.ReadLine());
-            while (n < 0)
+            int cnt = 0;
+            foreach (int x in arr)
             {
-                Console.WriteLine("Hãy nhập n > 0 !!!");
-                n = int.Parse(Console.ReadLine());
+                if (isPrime(x)) cnt++;
             }
+            return cnt;
         }
-        static long sumPrime(int n)
+
+        static bool isSquareNumber(int x) // hàm kiểm tra số chính phương 
         {
-            long sum = 0;
-            for (int i = 2; i < n; i++)
+            if (x <= 0) return false;
+            int k = (int)Math.Sqrt(x);
+            if (k * k == x) return true;
+            return false;
+        }
+        static int minSquareNumber(int[] arr) // hàm trả về số chính phương nhỏ nhất 
+        {
+            int minResult = -1;
+            foreach (int x in arr)
             {
-                if (isPrime(i))
+                if (isSquareNumber(x))
                 {
-                    sum += i;
+                    if (minResult == -1)
+                    {
+                        minResult = x;
+                    }
+                    else
+                    {
+                        minResult = Math.Min(x, minResult);
+                    }
                 }
             }
-            return sum;
+            return minResult;
         }
+
         static void Main(string[] args)
         {
-            Console.InputEncoding = Encoding.UTF8;
             Console.OutputEncoding = Encoding.UTF8;
-            Console.WriteLine("Mời bạn nhập vào số tự nhiên n dương");
-            int n = 0;
-            Input(out n);
+            Console.InputEncoding = Encoding.UTF8;
 
-            Console.WriteLine("Tổng các số nguyên tố bé hơn số tự nhiên n là " + sumPrime(n));
+            Console.WriteLine("Mời bạn nhập vào n phần tử: ");
+            int n = int.Parse(Console.ReadLine());
+
+            // Tạo mảng + sinh giá trị ngẫu nhiên. 
+            int[] arr = new int[n];
+            Random rd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = rd.Next(-100, 101); // Giá trị số nguyên ngẫu nhiên từ -100 đến 100. 
+            }
+
+            Console.WriteLine($"Mảng số nguyên {n} phần tử số nguyên ngẫu nhiên vừa tạo : ");
+            OutPutArr(arr);
+            Console.WriteLine();
+
+            // Xử lý yêu cầu đề bài: 
+            bool check = false;
+            int sumOfOddNumberinArr = sumOddSum(arr, ref check);
+            int theNumberOfPrimeinArr = countPrime(arr);
+            int theMinSquareNumber = minSquareNumber(arr);
+            if (check == false)
+            {
+                Console.WriteLine("a) Mảng không có số lẻ");
+            }
+            else
+            {
+                Console.WriteLine("a) Tổng số các số nguyên lẻ trong mảng là: " + sumOfOddNumberinArr);
+            }
+            Console.WriteLine("b) Số lượng số nguyên tố trong mảng là: " + theNumberOfPrimeinArr);
+            if (theMinSquareNumber == -1)
+            {
+                Console.WriteLine("c) Mảng không có số chính phương");
+            }
+            else
+            {
+                Console.WriteLine("c) Số chính phương nhỏ nhất trong mảng là: " + theMinSquareNumber);
+            }
+
             Console.ReadLine();
         }
     }
--- Bai02/Program.cs
+++ Bai02/Program.cs
@@ -1,141 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Schema;
-class Program
+
+namespace Bai02
 {
-    static bool checkDayMonthYear(int d, int m, int y) // Hàm kiểm tra ngày hợp lệ
+    internal class Program
     {
-        if (d <= 0 || d > 31 || m <= 0 || m > 12 || y <= 0)
+        static bool isPrime(int x)
         {
-            return false;
-        }
-        else
-        {
-            int daysInMonth = DateTime.DaysInMonth(y, m);
-            if (d < 1 || d > daysInMonth)
+            if (x < 2) return false;
+            for (int i = 2; i * i <= x; i++)
             {
-                return false;
+                if (x % i == 0) return false;
             }
             return true;
         }
-    }
-    static int daysInMonth(int m, int y)
-    {
-        if (m <= 0 || m > 12 || y <= 0)
-        {
-            return -1;
-        }
-        else
-        {
-            return DateTime.DaysInMonth(y, m);
-        }
-    }
 
-    static void daysOfWeek(int d, int m, int y)
-    {
-        if (checkDayMonthYear(d, m, y) == false)
+        static void Input(out int n)
         {
-            Console.WriteLine("Ngày nhập không hợp lệ !");
-        }
-        else
-        {
-            DateTime dt = new DateTime(y, m, d);
-            string res = "";
-            switch (dt.DayOfWeek)
+            n = int.Parse(Console.ReadLine());
+            while (n < 0)
             {
-                case DayOfWeek.Sunday: res = "Chủ nhật"; break;
-                case DayOfWeek.Monday: res = "Thứ hai"; break;
-                case DayOfWeek.Tuesday: res = "Thứ ba"; break;
-                case DayOfWeek.Wednesday: res = "Thứ tư"; break;
-                case DayOfWeek.Thursday: res = "Thứ năm"; break;
-                case DayOfWeek.Friday: res = "Thứ sáu"; break;
-                case DayOfWeek.Saturday: res = "Thứ bảy"; break;
+                Console.WriteLine("Hãy nhập n > 0 !!!");
+                n = int.Parse(Console.ReadLine());
             }
-            Console.WriteLine($"Ngày {dt:dd/MM/yyyy} là {res}");
         }
-    }
-    static void Main()
-    {
-        Console.OutputEncoding = Encoding.UTF8;
-        Console.InputEncoding = Encoding.UTF8;
-
-        // Bài 3 : Kiểm tra ngày tháng năm hợp lệ: 
-        Console.WriteLine("Bai03: Kiểm tra ngày tháng năm hợp lệ");
-        int d, m, y;
-        while (true)
+        static long sumPrime(int n)
         {
-            Console.Write("Mời nhập ngày: ");
-            d = int.Parse(Console.ReadLine());
-            Console.Write("Mời nhập tháng: ");
-            m = int.Parse(Console.ReadLine());
-            Console.Write("Mời nhập năm: ");
-            y = int.Parse(Console.ReadLine());
-
-            if (checkDayMonthYear(d, m, y))
+            long sum = 0;
+            Console.WriteLine($"Các số nguyên tố nhỏ hơn {n} là :");
+            Console.Write("[ ");
+            for (int i = 2; i < n; i++)
             {
-                Console.WriteLine($"Ngay {d:00}/{m:00}/{y} hợp lệ. ");
+                if (isPrime(i))
+                {
+                    Console.Write(i + " ");
+                    sum += i;
+                }
             }
-            else Console.WriteLine("Ngày nhập không hợp lệ !");
-
-            Console.WriteLine("Bạn có muốn tiếp tục kiểm tra ngày khác không?");
-            Console.WriteLine("Nhấn 'y' nếu muốn tiếp tục, nếu không muốn nhấn ký tự khác");
-            char q = Console.ReadKey().KeyChar;
-            Console.WriteLine();
-            Console.ReadLine();
-            if (q != 'y') break;
+            Console.WriteLine("]");
+            return sum;
         }
-
-        // Bài 4 : Cho biết số ngày trong tháng, năm. 
-        Console.WriteLine("Bai04: In Ra số ngày trong tháng, năm đã nhập");
-        while (true)
+        static void Main(string[] args)
         {
-            Console.Write("Mời nhập tháng: ");
-            m = int.Parse(Console.ReadLine());
-            Console.Write("Mời nhập năm: ");
-            y = int.Parse(Console.ReadLine());
-
-            int res = daysInMonth(m, y);
-            if (res == -1)
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            int n = 0;
+            while (true)
             {
-                Console.WriteLine("Tháng, năm nhập vào không hợp lệ!");
+                Console.WriteLine("Mời bạn nhập vào số tự nhiên n dương");
+
+                Input(out n);
+
+                Console.WriteLine("Tổng các số nguyên tố nhỏ hơn số tự nhiên n là " + sumPrime(n));
+                Console.ReadLine();
+
+                Console.WriteLine("Nhấn 'y' nếu muốn tiếp tục, nếu không muốn nhấn ký tự khác");
+                char q = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                Console.ReadLine();
+                if (q != 'y') break;
+
             }
-            else
-            {
-                Console.WriteLine($"Tháng {m} năm {y} có {res} ngày.");
-            }
-
-            Console.WriteLine("Nhấn 'y' nếu muốn tiếp tục, nếu không muốn nhấn ký tự khác");
-            char q = Console.ReadKey().KeyChar;
-            Console.WriteLine();
-            Console.ReadLine();
-            if (q != 'y') break;
-        }
-
-        // Bài 05: Cho biết ngày tháng năm nhập vào là thứ mấy. 
-        Console.WriteLine("Bai05: In ra thứ trong tuần của ngày tháng năm đã nhập");
-        while (true)
-        {
-            Console.Write("Mời nhập ngày: ");
-            d = int.Parse(Console.ReadLine());
-            Console.Write("Mời nhập tháng: ");
-            m = int.Parse(Console.ReadLine());
-            Console.Write("Mời nhập năm: ");
-            y = int.Parse(Console.ReadLine());
-
-            daysOfWeek(d, m, y);
-
-            Console.WriteLine("Nhấn 'y' nếu muốn tiếp tục, nếu không muốn nhấn ký tự khác");
-            char q = Console.ReadKey().KeyChar;
-            Console.WriteLine();
-            Console.ReadLine();
-            if (q != 'y') break;
         }
     }
-
 }
